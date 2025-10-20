2dfcdff8ab13f97e248e9ebe329523c14686835e
Kenneth Obsequio <k80308392@gmail.com>
2025-10-20 21:34:05 +0800

    fix: CORS
    missing CORS again.
    
    Signed-off-by: Kenneth Obsequio <k80308392@gmail.com>

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const app = express();
 // JSON Payload
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true,
+}));
 app.use(cookieParser());
 
 // Load all
