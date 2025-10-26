1b86cdf8b84ffadb5d959e9eae32f1ddd0f1a76a
Kenneth Obsequio <k80308392@gmail.com>
2025-10-26 12:56:56 +0800

    fix: user auth
    Listening to Weight of the World.
    
    Signed-off-by: Kenneth Obsequio <k80308392@gmail.com>

--- src/modules/user_creds/user_creds.srv.js
+++ src/modules/user_creds/user_creds.srv.js
@@ -81,11 +81,9 @@ class UserCredsService extends UserSessionsService {
       res.cookie('refreshToken', refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // 'None' for cross-site
-        path: '/',
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        sameSite: 'Strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000,
       });
-
 
       // Return success response
       return {
