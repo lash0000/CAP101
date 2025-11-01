6fd288c9cabf55c9c9b25bf49dba884e364ba363
Kenneth Obsequio <k80308392@gmail.com>
2025-11-01 12:00:10 +0800

    cors: add locale
    Looks fine after all.
    
    Signed-off-by: Kenneth Obsequio <k80308392@gmail.com>

--- server.js
+++ server.js
@@ -17,7 +17,10 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 // CORS
 app.use(
   cors({
-    origin: "https://barangay-santa-monica.vercel.app",
+    origin: [
+      "https://barangay-santa-monica.vercel.app",
+      "http://localhost:5173",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Accept"],
