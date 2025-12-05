a4b9a1713f72e6727b5e7d133411a96627da9341
Kenneth Obsequio <k80308392@gmail.com>
2025-12-05 11:56:36 +0800

    add: latest domain
    For business purposes.
    
    Signed-off-by: Kenneth Obsequio <k80308392@gmail.com>

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(
       "http://localhost:5173",
     ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Accept"],
   })
 );
